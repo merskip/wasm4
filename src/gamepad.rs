2c3b7de53ee4946a608620bd305932510641258d
Piotr Merski <merskip@gmail.com>
2023-03-04 13:48:04 +0100

    Added is_pressed, is_held and is_released methods for Gamepad

--- src/gamepad.rs
+++ src/gamepad.rs
@@ -1,12 +1,15 @@
 use core::ops::BitAnd;
 use crate::system;
 
+#[derive(Eq, PartialEq)]
 pub struct Gamepad {
-    address: *const u8
+    address: *const u8,
+    last_state: u8
 }
 
 #[repr(u8)]
 #[allow(dead_code)]
+#[derive(Copy, Clone)]
 pub enum GamepadButton {
     ButtonX = system::BUTTON_1,
     ButtonY = system::BUTTON_2,
@@ -19,7 +22,7 @@ pub enum GamepadButton {
 #[allow(dead_code)]
 impl Gamepad {
     unsafe fn new(address: *const u8) -> Self {
-        Gamepad { address }
+        Gamepad { address, last_state: 0 }
     }
 
     pub fn gamepad1() -> Self {
@@ -37,10 +40,31 @@ impl Gamepad {
     pub fn gamepad4() -> Self {
         unsafe { Gamepad::new(system::GAMEPAD4) }
     }
+}
+
+impl Gamepad {
+    pub fn late_update(&mut self) {
+        unsafe {
+            self.last_state = *self.address;
+        }
+    }
 
     pub fn is_pressed(&self, button: GamepadButton) -> bool {
-        unsafe {
-            (*self.address).bitand(button as u8) != 0
-        }
+        let state = unsafe { *self.address };
+        !Self::is_pressing(self.last_state, &button) && Self::is_pressing(state, &button)
+    }
+
+    pub fn is_held(&self, button: GamepadButton) -> bool {
+        let state = unsafe { *self.address };
+        Self::is_pressing(state, &button)
+    }
+
+    pub fn is_released(&self, button: GamepadButton) -> bool {
+        let state = unsafe { *self.address };
+        Self::is_pressing(self.last_state, &button) && !Self::is_pressing(state, &button)
+    }
+
+    fn is_pressing(state: u8, button: &GamepadButton) -> bool {
+        state.bitand(*button as u8) != 0
     }
 }
